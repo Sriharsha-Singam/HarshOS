b02e033138fda3eab55b5f8ee9357dcadab49db7
Sriharsha Singam <harshasingam3@gmail.com>
2020-06-22 04:49:26 +0000

    added-heap-linked-list-getting-started

--- memory/kernel_heap.c
+++ memory/kernel_heap.c
@@ -3,3 +3,60 @@
 //
 
 #include "kernel_heap.h"
+#include "../memory/kernel_malloc.h"
+
+
+/**
+ *  This address will change each time more of the heap is allocated/deallocated.
+ *  The HEAP will move backwards from address 0xC0000000 for EVERY PROCESS -- not just the kernel.
+ *  But the HEAP will be mapped to different PHYSICAL PAGES with similar/same VIRTUAL ADDRESSES
+ */
+u32 CURRENT_HEAP_ADDRESS = HEAP_START_POINT;
+
+/**
+ * This variable will be used to hold to location for HEAP Specified Pages.
+ * These page may leave out massive chunks of memory in between. This might be
+ * a good way to keep track of that.
+ *
+ * Another option is to potentially check the last address of the last
+ * heap_entry but this might lead to some inconsistencies.
+ */
+u32 CURRENT_HEAP_ADDRESS_PAGE_ALIGNED = HEAP_START_POINT;
+
+
+heap_entry_linked_list* HEAP_LINKED_LIST;
+
+/**
+ * This function will insert a new Linked List Heap Entry
+ *
+ * @param size This will be the size of the space that needs to be
+ *             heap malloced.
+ *
+ * @return HEAP_ENTRY_LINKED_LIST POINTER
+ */
+heap_entry_linked_list* insert_heap_entry(u32 size) {
+
+    heap_entry_linked_list* new_heap_entry_linked_list = (heap_entry_linked_list*) CURRENT_HEAP_ADDRESS;
+
+    if (!HEAP_LINKED_LIST) {
+        HEAP_LINKED_LIST = new_heap_entry_linked_list;
+        new_heap_entry_linked_list->previous = (heap_entry_linked_list*) NULL;
+        new_heap_entry_linked_list->next = (heap_entry_linked_list*) NULL;
+    } else {
+
+        heap_entry_linked_list* linked_list_copy = new_heap_entry_linked_list;
+
+        while(linked_list_copy->next) {
+            linked_list_copy = linked_list_copy->next;
+        }
+
+        linked_list_copy->next = new_heap_entry_linked_list;
+        new_heap_entry_linked_list->previous = linked_list_copy;
+    }
+
+    new_heap_entry_linked_list->heap_entry_size = size;
+
+    CURRENT_HEAP_ADDRESS = CURRENT_HEAP_ADDRESS - sizeof(heap_entry_linked_list);
+
+    return new_heap_entry_linked_list;
+}
--- memory/kernel_heap.h
+++ memory/kernel_heap.h
@@ -5,6 +5,48 @@
 #ifndef HARSHOS_KERNEL_HEAP_H
 #define HARSHOS_KERNEL_HEAP_H
 
+#include "../cpu/types.h"
 
+#define HEAP_MAGIC_NUMBER 0xF92BD3A5
+#define HEAP_START_POINT 0xC0000000
+//#define KHEAP_INITIAL_SIZE  0x100000
+//#define HEAP_INDEX_SIZE   0x20000
+//#define HEAP_MIN_SIZE     0x70000
+
+extern u32 CURRENT_HEAP_ADDRESS;
+
+typedef struct _heap_entry_linked_list heap_entry_linked_list;
+
+struct _heap_entry_linked_list {
+    void* heap_entry_location;
+    u32 heap_entry_size;
+    heap_entry_linked_list* next;
+    heap_entry_linked_list* previous;
+};
+
+
+typedef struct _heap_entry_header_t heap_entry_header_t;
+
+struct _heap_entry_header_t{
+    u32 magic_number;
+    u8 is_hole;
+    u32 size;
+};
+
+
+typedef struct _heap_entry_footer_t heap_entry_footer_t;
+
+struct _heap_entry_footer_t {
+    u32 magic_number;
+    heap_entry_header_t* header_location;
+};
+
+
+void* kernel_heap_malloc(u32 size);
+void* kernel_heap_malloc_page_aligned(u32 size);
+void* kernel_heap_calloc(u32 size);
+void* kernel_heap_calloc_page_aligned(u32 size);
+
+void kernel_heap_free(void* address);
 
 #endif //HARSHOS_KERNEL_HEAP_H
